469cd01d7109d10dfbb0a98916655d317552591f
Ilya_Gloshkin <ilyagosh@yandex.ru>
2026-03-28 20:35:55 +0500

    Добавлено отображение координат робота

--- robots/src/gui/GameWindow.java
+++ robots/src/gui/GameWindow.java
@@ -13,11 +13,14 @@ public class GameWindow extends JInternalFrame
 
         GameModel m_gameModel = new GameModel();
         GameView m_gameView = new GameView(m_gameModel);
+        BotCoordinatesVisualizer m_BCV = new BotCoordinatesVisualizer(m_gameModel);
 
         m_gameModel.addListener(m_gameView);
+        m_gameModel.addListener(m_BCV);
 
         JPanel panel = new JPanel(new BorderLayout());
         panel.add(m_gameView, BorderLayout.CENTER);
+        panel.add(m_BCV, BorderLayout.SOUTH);
         getContentPane().add(panel);
         pack();
     }
